7d288cd25606864017ff22e53e6fbb9860a5b9a0
Calvin Rousseau <calvinrousseau90@gmail.com>
2024-09-06 10:15:35 +0200

    add tic tac toe

---
+++ test.js
@@ -0,0 +1,158 @@
+// Savoir quel joueur doit jouer
+let currentPlayer = 1;
+// Vérifier si un joueur a gagné
+let isGameWon = false;
+// Vérifier si la partie est finie
+let isGameFinished = false;
+// La grille du morpion
+let grid = [0, 0, 0, 
+            0, 0, 0, 
+            0, 0, 0];
+
+const player1 = `<i class="fa-solid fa-x"></i>`;
+const player2 = `<i class="fa-solid fa-o"></i>`;
+
+const score = {
+    player1: 0,
+    player2: 0,
+    draw: 0
+}
+
+function init(){
+    checkIfSomeoneWon()
+    // Ajoute la fonction qui permet de reset la partie
+    
+}
+
+
+function initScore(){
+    document.querySelector('#j1').innerHTML = score.player1
+    document.querySelector('#draw').innerHTML = score.draw
+    document.querySelector('#j2').innerHTML = score.player2
+}
+
+// Fonction qui permet d'afficher le symbole du joueur sur le DOM
+function displayPlayerSymbol(index){
+    document.querySelectorAll(".cell")[index].innerHTML = currentPlayer === 1 ? player1 : player2;
+}
+
+// On doit vérifier s'il y a un vainqueur
+function checkIfSomeoneWon(){
+
+    // Définir les combinaisons gagnantes
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Lignes
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colonnes
+        [0, 4, 8], [2, 4, 6] // Diagonales
+    ]
+
+    // Fonction qui vérifie si une combinaison données est gagnante
+    const isWinningCombination = (combination) => 
+        combination.every(index => grid[index] === currentPlayer)
+
+    // On vérifie si une combinaison gagnante est présente
+    isGameWon = winningCombinations.some(isWinningCombination);
+    if(isGameWon) {
+        if(currentPlayer === 1){
+            score.player1++
+        } else  {
+            score.player2++
+        }
+    }
+    // On vérifie si la partie est finie
+    isGameFinished = grid.every(cell => cell !== 0);
+
+    // Si personne n'a gagné et que le jeu n'est PAS terminé : on change le joueur
+    if(!isGameWon && !isGameFinished){
+        changePlayer()
+    }
+
+    // Si le jeu est terminé sans vainqueur
+    if(isGameFinished && !isGameWon){
+        score.draw++
+    }
+
+    // Fonction qui permet de mettre à jour les scores
+    initScore()
+}
+
+// Fonction qui pemet de changer le tour du joueur
+function changePlayer(){
+    // currentPlayer = currentPlayer === 1 ? 2 : 1;
+    if (currentPlayer === 1){
+        playWithDebillo()
+        currentPlayer = 2
+    } else {
+        currentPlayer = 1
+    }
+
+    // Si le joueur est le joueur 2, on doit lui permettre de jouer avec l'IA Debilla
+    if(currentPlayer === 2){
+        playWithDebilla()
+    }
+}
+
+function playWithDebillo(){
+    let randomIndex = Math.floor(Math.random() * 9);
+    if(grid[randomIndex] === 0){
+        // La case est vide, on peut jouer
+        grid[randomIndex] = currentPlayer;
+
+        // On affiche le symbole du joueur sur le DOM
+        displayPlayerSymbol(randomIndex)
+        // On test pour savoir si quelqu'un a gagné
+        checkIfSomeoneWon()
+    } else {
+        // La case n'est pas vide, on recommence
+        playWithDebillo()
+    }
+}
+
+// Fonction qui permet de jouer avec l'IA Debilla
+function playWithDebilla(){
+    // Faire le code pour que Debilla puisse jouer
+
+    // On choisi un nombre alétatoire entre 0 et 8
+    let randomIndex = Math.floor(Math.random() * 9);
+    console.log('randomIndex ', randomIndex)
+
+    // On vérifie si la case est vide
+    if(grid[randomIndex] === 0){
+        // La case est vide, on peut jouer
+        grid[randomIndex] = currentPlayer;
+
+        // On affiche le symbole du joueur sur le DOM
+        displayPlayerSymbol(randomIndex)
+        // On test pour savoir si quelqu'un a gagné
+        checkIfSomeoneWon()
+    } else {
+        // La case n'est pas vide, on recommence
+        playWithDebilla()
+    }
+}
+
+// Fonction qui permet de recommencer une partie
+function newGame(){
+    currentPlayer = 1;
+    isGameWon = false;
+    isGameFinished = false;
+    grid = [0, 0, 0, 
+            0, 0, 0, 
+            0, 0, 0];
+
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.innerHTML = '';
+    });
+}
+
+const btnStart = document.querySelector("#startIa")
+
+btnStart.addEventListener('click', (e) => {
+    init()
+})
+
+const btnReset = document.querySelector("#reset");
+btnReset.addEventListener('click', (e) => {
+    newGame()
+})
